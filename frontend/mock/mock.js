b265f8c66714b814021fd6f260582f5417dbc958
jooam <lord0325@naver.com>
2022-07-12 16:37:28 +0900

    test: Set Mocking Data
    - 서버로 초기요청을 보냈을 때 오는 가상의 데이터 설정

--- frontend/mock/mock.js
+++ frontend/mock/mock.js
@@ -1,71 +1,61 @@
-const mockData = {
-	column: {
-		1: '해야할 일',
-		2: '진행중인 일',
-		3: '완료한 일',
+const processedData = {
+	1: {
+		title: '해야할 일',
+		todos: [
+			{
+				id: 1,
+				title: '코딩하기',
+				content: '코딩을 사랑해',
+				column_id: 1,
+				index: 0,
+			},
+			{
+				id: 4,
+				title: '코딩하기',
+				content: '코딩을 사랑해',
+				column_id: 1,
+				index: 1,
+			},
+			{
+				id: 5,
+				title: '춤추기',
+				content: '하이',
+				column_id: 1,
+				index: 2,
+			},
+		],
 	},
-	todos: [
-		{
-			id: 1,
-			title: '코딩하기',
-			content: '코딩을 사랑해',
-			column_id: 1,
-			index: 0,
-		},
-		{
-			id: 2,
-			title: '코딩하기',
-			content: '코딩을 사랑해',
-			column_id: 2,
-			index: 1,
-		},
-		{
-			id: 3,
-			title: '코딩하기',
-			content: '코딩을 사랑해',
-			column_id: 3,
-			index: 2,
-		},
-		{
-			id: 4,
-			title: '코딩하기',
-			content: '코딩을 사랑해',
-			column_id: 1,
-			index: 3,
-		},
-	],
-	logs: [
-		{
-			id: 1,
-			userName: '한진탁',
-			content: '코딩하기',
-			date: new Date(),
-		},
-		{
-			id: 2,
-			userName: '한진탁',
-			content: '코딩하기',
-			date: new Date(),
-		},
-		{
-			id: 3,
-			userName: '한진탁',
-			content: '코딩하기',
-			date: new Date(),
-		},
-		{
-			id: 4,
-			userName: '한진탁',
-			content: '코딩하기',
-			date: new Date(),
-		},
-		{
-			id: 5,
-			userName: '한진탁',
-			content: '코딩하기',
-			date: new Date(),
-		},
-	],
+	2: {
+		title: '진행중인 일',
+		todos: [
+			{
+				id: 2,
+				title: '코딩하기',
+				content: '코딩을 사랑해',
+				column_id: 2,
+				index: 0,
+			},
+		],
+	},
+	3: {
+		title: '완료된 일',
+		todos: [
+			{
+				id: 3,
+				title: '코딩하기',
+				content: '코딩을 사랑해',
+				column_id: 3,
+				index: 0,
+			},
+			{
+				id: 6,
+				title: '커피사기',
+				content: '내일까지 프론트',
+				column_id: 3,
+				index: 1,
+			},
+		],
+	},
 };
 
-export default mockData;
+export default processedData;
