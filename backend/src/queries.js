e81ba3b427ad10c55654c5834d8d12126625aab5
jintak0401 <jintak0401@naver.com>
2022-07-15 01:45:45 +0900

    fix: todo move 시 뒤에 있는 todo들의 index 를 수정

--- backend/src/queries.js
+++ backend/src/queries.js
@@ -1,166 +1,180 @@
 import {
-  columnData,
-  logData,
-  removeDataAtDB,
-  todoData,
-  userData,
+    columnData,
+    logData,
+    removeDataAtDB,
+    todoData,
+    userData,
 } from "../mock/mock.js";
 
-function getUserFromDB({ userId }) {
-  return userData[userId];
+function getUserFromDB({userId}) {
+    return userData[userId];
 }
 
-function readLogFromDB({ userId }) {
-  return logData.filter((log) => log.userId === userId);
+function readLogFromDB({userId}) {
+    return logData.filter((log) => log.userId === userId);
 }
 
 function insertLogFromDB({
-  userId,
-  type,
-  todoTitle,
-  todoColumnId,
-  textTodoColumnId,
-}) {
-  logData.push({
-    id: logData.length + 1,
     userId,
     type,
     todoTitle,
     todoColumnId,
     textTodoColumnId,
-    date: new Date(),
-  });
+}) {
+    logData.push({
+        id: logData.length + 1,
+        userId,
+        type,
+        todoTitle,
+        todoColumnId,
+        textTodoColumnId,
+        date: new Date(),
+    });
 }
 
 // targetTodo: { title, content }
-function insertTodoFromDB({ todoColumnId, targetTodo, userId }) {
-  // 먼저 있던 todo 들의 index를 하나씩 늘려준다
-  todoData
-    .filter((todo) => todo.todoColumnId === todoColumnId)
-    .forEach((todo) => todo.index++);
-  const todoId = todoData.length;
-  targetTodo = { ...targetTodo, id: todoId, index: 0, todoColumnId };
-  todoData.push(targetTodo);
+function insertTodoFromDB({todoColumnId, targetTodo, userId}) {
+    // 먼저 있던 todo 들의 index를 하나씩 늘려준다
+    todoData
+        .filter((todo) => todo.todoColumnId === todoColumnId)
+        .forEach((todo) => todo.index++);
+    const todoId = todoData.length;
+    targetTodo = {...targetTodo, id: todoId, index: 0, todoColumnId};
+    todoData.push(targetTodo);
 
-  /********** 로그 기록 **********/
-  insertLogFromDB({
-    userId,
-    type: "add",
-    todoTitle: targetTodo.title,
-    todoColumnId,
-    nextTodoColumnId: null,
-  });
-  console.log(logData.at(-1));
-  /***************************/
+    /********** 로그 기록 **********/
+    insertLogFromDB({
+        userId,
+        type: "add",
+        todoTitle: targetTodo.title,
+        todoColumnId,
+        nextTodoColumnId: null,
+    });
+    console.log(logData.at(-1));
+    /***************************/
 
-  // 프론트에서 쓸 수 있는 key 값을 return 해준다.
-  return todoId;
+    // 프론트에서 쓸 수 있는 key 값을 return 해준다.
+    return todoId;
 }
 
-function removeTodoFromDB({ todoId, userId }) {
-  const removedTodo = todoData.find((todo) => todo.id === todoId);
-  removeDataAtDB(todoId);
+function removeTodoFromDB({todoId, userId}) {
+    const removedTodo = todoData.find((todo) => todo.id === todoId);
+    removeDataAtDB(todoId);
 
-  /********** 로그 기록 **********/
-  insertLogFromDB({
-    userId,
-    type: "remove",
-    todoTitle: removedTodo.title,
-    todoColumnId: removedTodo.todoColumnId,
-    nextTodoColumnId: null,
-  });
-  console.log(logData.at(-1));
-  /***************************/
+    /********** 로그 기록 **********/
+    insertLogFromDB({
+        userId,
+        type: "remove",
+        todoTitle: removedTodo.title,
+        todoColumnId: removedTodo.todoColumnId,
+        nextTodoColumnId: null,
+    });
+    console.log(logData.at(-1));
+    /***************************/
 
-  return removedTodo;
+    return removedTodo;
 }
 
-function moveTodoFromDB({ todoId, nextTodoColumnId, userId }) {
-  const target = todoData.find((todo) => todo.id === todoId);
-  const originTodoColumnId = target.todoColumnId;
-  target.todoColumnId = nextTodoColumnId;
+function moveTodoFromDB({todoId, nextTodoColumnId, userId, nextIndex}) {
+    const target = todoData.find((todo) => todo.id === todoId);
+    const originIndex = target.index;
+    const originTodoColumnId = target.todoColumnId;
+    todoData.filter(todo => todo.todoColumnId === originTodoColumnId && todo.index > target.index)
+        .forEach(todo => todo.index--);
+    todoData.filter(todo => todo.todoColumnId === nextTodoColumnId && todo.index >= nextIndex)
+        .forEach(todo => todo.index++);
+    target.todoColumnId = nextTodoColumnId;
+    target.index = nextIndex;
 
-  /********** 로그 기록 **********/
-  insertLogFromDB({
-    userId,
-    type: "move",
-    todoTitle: target.title,
-    todoColumnId: originTodoColumnId,
-    nextTodoColumnId: nextTodoColumnId,
-  });
-  console.log(logData.at(-1));
-  /***************************/
+    /********** 로그 기록 **********/
+    insertLogFromDB({
+        userId,
+        type: "move",
+        todoTitle: target.title,
+        todoColumnId: originTodoColumnId,
+        nextTodoColumnId: nextTodoColumnId,
+    });
+    console.log(logData.at(-1));
+    /***************************/
 
-  return target;
+    return target;
 }
 
-function updateTodoFromDB({ todoId, targetTodo, userId }) {
-  const curTodo = todoData.find((todo) => todo.id === todoId);
-  const originTitle = curTodo.title;
-  Object.keys(targetTodo).forEach((key) => (curTodo[key] = targetTodo[key]));
+function updateTodoFromDB({todoId, targetTodo, userId}) {
+    const curTodo = todoData.find((todo) => todo.id === todoId);
+    const originTitle = curTodo.title;
+    Object.keys(targetTodo)
+        .forEach((key) => (
+            curTodo[key] = targetTodo[key]
+        ));
 
-  /********** 로그 기록 **********/
-  insertLogFromDB({
-    userId,
-    type: "update",
-    todoTitle: originTitle,
-    todoColumnId: curTodo.todoColumnId,
-    nextTodoColumnId: null,
-  });
-  console.log(logData.at(-1));
-  /***************************/
+    /********** 로그 기록 **********/
+    insertLogFromDB({
+        userId,
+        type: "update",
+        todoTitle: originTitle,
+        todoColumnId: curTodo.todoColumnId,
+        nextTodoColumnId: null,
+    });
+    console.log(logData.at(-1));
+    /***************************/
 
-  return curTodo;
+    return curTodo;
 }
 
-function readTodoFromDB({ userId }) {
-  const columnIds = getUsersColumnFromDB({ userId }).map(({ id }) => id);
-  return todoData.filter((todo) => columnIds.includes(todo.todoColumnId));
+function readTodoFromDB({userId}) {
+    const columnIds = getUsersColumnFromDB({userId})
+        .map(({id}) => id);
+    return todoData.filter((todo) => columnIds.includes(todo.todoColumnId));
 }
 
-function insertColumnFromDB() {}
-
-function updateColumnFromDB({ todoColumnId, nextColumnTitle }) {
-  const targetColumn = columnData.find((column) => column.id === todoColumnId);
-  targetColumn.title = nextColumnTitle;
-  return targetColumn;
+function insertColumnFromDB() {
 }
 
-function deleteColumnFromDB() {}
-
-function readColumnFromDB({ todoColumnId }) {
-  return columnData.find((column) => column.id === todoColumnId);
+function updateColumnFromDB({todoColumnId, nextColumnTitle}) {
+    const targetColumn = columnData.find((column) => column.id === todoColumnId);
+    targetColumn.title = nextColumnTitle;
+    return targetColumn;
 }
 
-function isUsersColumn({ todoColumnId, userId }) {
-	let tmp;
-  return (
-    (tmp = columnData.find((column) => column.id === todoColumnId)) && tmp.userId === userId
-  );
+function deleteColumnFromDB() {
 }
 
-function isUsersTodo({ todoId, userId }) {
-  const targetTodo = todoData.find((todo) => todo.id === todoId);
-  const usersColumn = columnData.filter((column) => column.userId === userId);
-  return targetTodo && targetTodo.todoColumnId in usersColumn;
+function readColumnFromDB({todoColumnId}) {
+    return columnData.find((column) => column.id === todoColumnId);
 }
 
-function getUsersColumnFromDB({ userId }) {
-  return columnData.filter((column) => column.userId === userId);
+function isUsersColumn({todoColumnId, userId}) {
+    let tmp;
+    return (
+        (
+            tmp = columnData.find((column) => column.id === todoColumnId)
+        ) && tmp.userId === userId
+    );
+}
+
+function isUsersTodo({todoId, userId}) {
+    const targetTodo = todoData.find((todo) => todo.id === todoId);
+    const usersColumn = columnData.filter((column) => column.userId === userId);
+    return targetTodo && usersColumn.includes(targetTodo.todoColumnId);
+}
+
+function getUsersColumnFromDB({userId}) {
+    return columnData.filter((column) => column.userId === userId);
 }
 
 export {
-  getUsersColumnFromDB,
-  isUsersTodo,
-  isUsersColumn,
-  getUserFromDB,
-  readLogFromDB,
-  insertLogFromDB,
-  moveTodoFromDB,
-  removeTodoFromDB,
-  updateTodoFromDB,
-  insertTodoFromDB,
-  readTodoFromDB,
-  readColumnFromDB,
-  updateColumnFromDB,
+    getUsersColumnFromDB,
+    isUsersTodo,
+    isUsersColumn,
+    getUserFromDB,
+    readLogFromDB,
+    insertLogFromDB,
+    moveTodoFromDB,
+    removeTodoFromDB,
+    updateTodoFromDB,
+    insertTodoFromDB,
+    readTodoFromDB,
+    readColumnFromDB,
+    updateColumnFromDB,
 };
