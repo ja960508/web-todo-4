1092480346b07d4e749cd34d9abb525aac4b7e98
jooam <lord0325@naver.com>
2022-07-14 13:52:18 +0900

    feat: Drag 이벤트 등록
    - 고스트 이미지 생성
    - mouse 동작에 따라 각각의 이벤트 메소드 등록
    - 적용된 inline 스타일 해제 후 리스트에 삽입

--- frontend/src/js/components/TodoContainer/TodoColumn/TodoColumn.js
+++ frontend/src/js/components/TodoContainer/TodoColumn/TodoColumn.js
@@ -1,6 +1,7 @@
 import Component from '../../../core/Component';
 import TodoColumnHeader from './TodoColumnHeader';
 import TodoList from './TodoList';
+import { mouseUp, moveAt, onMouseMove } from '../../../events/todoDragEvent';
 
 class TodoColumn extends Component {
 	constructor(...data) {
@@ -18,6 +19,50 @@ class TodoColumn extends Component {
 		new TodoList('ul', this.$target, {
 			class: ['todo-list'],
 			todos: todos,
+		});
+	}
+
+	setEvent() {
+		let handleMouseMove = null;
+		let todoCard = null;
+
+		this.addEvent('mousedown', '.column', (e) => {
+			todoCard = e.target.closest('li');
+
+			if (!todoCard) {
+				return;
+			}
+
+			const ghost = todoCard.cloneNode(true);
+			todoCard.classList.add('dragging');
+
+			handleMouseMove = onMouseMove(ghost);
+
+			ghost.style.position = 'absolute';
+			ghost.style.zIndex = 1000;
+			ghost.style.width = window.getComputedStyle(todoCard).width;
+			ghost.style.zIndex = window.getComputedStyle(todoCard).height;
+
+			document.body.appendChild(ghost);
+
+			moveAt(ghost, e.pageX, e.pageY);
+
+			document.addEventListener('mousemove', handleMouseMove);
+
+			document.addEventListener(
+				'mouseup',
+				(e) => {
+					todoCard.classList.remove('dragging');
+					document.removeEventListener('mousemove', handleMouseMove);
+
+					if (!mouseUp(e)) {
+						return;
+					}
+
+					todoCard.parentNode.removeChild(todoCard);
+				},
+				{ once: true }
+			);
 		});
 	}
 }
