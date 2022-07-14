ed3e04f13fa8402e3538745ff98b46aea1c3a963
jooam <lord0325@naver.com>
2022-07-14 13:39:48 +0900

    feat: mousemove 이벤트 구현
    - 설정된 타겟에 따라 mousemove

---
+++ frontend/src/js/events/todoDragEvent.js
@@ -0,0 +1,10 @@
+export function moveAt(target, pageX, pageY) {
+	target.style.left = pageX - target.offsetWidth / 2 + 'px';
+	target.style.top = pageY - target.offsetHeight / 2 + 'px';
+}
+
+export function onMouseMove(target) {
+	return (event) => {
+		moveAt(target, event.pageX, event.pageY);
+	};
+}
