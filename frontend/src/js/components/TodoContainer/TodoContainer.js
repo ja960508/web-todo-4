681ed9aa32aa7423855768f78b51b55866d25198
jintak0401 <jintak0401@naver.com>
2022-07-14 11:35:29 +0900

    feat: addForm & todoCard 생성메소드 구현
    - createTodoCard(): todoCard 생성
    - createAddForm(): addForm 생성
    - handleTodoCard 객체로 각 상황별 함수 매핑

--- frontend/src/js/components/TodoContainer/TodoContainer.js
+++ frontend/src/js/components/TodoContainer/TodoContainer.js
@@ -1,37 +1,72 @@
 import Component from '../../core/component';
 import processedData from '../../../../mock/mock';
 import TodoColumn from './TodoColumn/TodoColumn';
+import TodoAddForm from './TodoAddForm';
+import TodoCard from './TodoColumn/TodoCard';
 
 class TodoContainer extends Component {
 	constructor(...data) {
 		super(...data);
+		this.columns = {};
+		this.addForm = null;
+		this.prevCard = null;
 	}
 
 	setup() {
 		const data = new Promise((resolve) => resolve(processedData)).then(
 			(res) => {
-				this.setState({ columnsData: res });
+				const columnData = {};
+				Object.keys(res).forEach((key) => (columnData[key] = { ...res[key] }));
+				this.setState({ columnData });
 			}
 		);
 	}
 
 	setChildren() {
 		this.clearChildren();
+		this.columns = {};
 
-		this.state.columnsData &&
-			Object.keys(this.state.columnsData).forEach(
-				(key) =>
-					new TodoColumn('div', this.$target, {
-						class: ['column'],
-						columnData: this.state.columnsData[key],
-						showTodoAddForm: this.showTodoAddForm,
-					})
-			);
+		if (!this.state.columnData) {
+			return;
+		}
+
+		for (const key in this.state.columnData) {
+			this.columns[key] = new TodoColumn('div', this.$target, {
+				class: ['column'],
+				dataset: {
+					columnKey: key,
+				},
+				columnData: this.state.columnData[key],
+			});
+		}
 	}
 
-	showTodoAddForm(columnId) {
-		console.log(`${columnId} 생성`);
+	createAddForm({ $parent, props = {}, $beforeElement }) {
+		const classList = [...(props.class || []), 'todo-add-form'];
+		return new TodoAddForm(
+			'li',
+			$parent,
+			{ ...props, class: classList },
+			$beforeElement
+		);
 	}
+
+	createTodoCard({ $parent, props = {}, $beforeElement }) {
+		const classList = [...(props.class || []), 'todo-card'];
+		return new TodoCard(
+			'li',
+			$parent,
+			{ ...props, class: classList },
+			$beforeElement
+		);
+	}
+
+	handleTodoCard = {
+		editStart: this.createAddForm,
+		addStart: this.createAddForm,
+		confirmEditTodo: this.createTodoCard,
+		confirmAddTodo: this.createTodoCard,
+	};
 }
 
 export default TodoContainer;
